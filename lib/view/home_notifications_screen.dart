a4cf3171423fe6dfa8cce6258b5dff631dbf03ea
jaydipnasit-oceanmtech <145960611+jaydipnasit-oceanmtech@users.noreply.github.com>
2023-12-01 11:15:28 +0530

    ui changin

--- lib/view/home_notifications_screen.dart
+++ lib/view/home_notifications_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker_bdaya/flutter_datetime_picker_bdaya.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intl/intl.dart';
 
 import 'package:notifications/view/servies.dart';
@@ -16,13 +17,15 @@ class _HomeNotificationsState extends State<HomeNotifications> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
         Center(
             child: scheduleTime != null
                 ? Text(DateFormat('dd/MM/yyyy | hh:mm a').format(scheduleTime!))
                 : const SizedBox.shrink()),
         const SizedBox(height: 10),
-        ElevatedButton(
+        commonButton(
+          icon: Icons.date_range,
           onPressed: () {
             DatePickerBdaya.showDateTimePicker(
               context,
@@ -31,10 +34,9 @@ class _HomeNotificationsState extends State<HomeNotifications> {
               onConfirm: (date) {},
             );
           },
-          child: const Text("Datepick"),
+          text: "Datepick",
         ),
-        ElevatedButton(
-            child: const Text('Schedule notifications'),
+        commonButton(
             onPressed: () {
               if (scheduleTime != null) {
                 debugPrint('Notification Scheduled for $scheduleTime');
@@ -54,21 +56,39 @@ class _HomeNotificationsState extends State<HomeNotifications> {
                   );
                 });
               }
-            }),
-        ElevatedButton(
-            onPressed: () {
-              NotificationService().showNotification(
-                title: "Flutter Devloper",
-                body: "It working",
-              );
             },
-            child: const Text("Instance Notification")),
-        ElevatedButton(
-            onPressed: () {
-              NotificationService().scheduleNotificationCancel();
-            },
-            child: const Text("Schedule Cancel"))
+            icon: Icons.schedule_send,
+            text: "Schedule notifications"),
+        commonButton(
+          onPressed: () {
+            NotificationService().showNotification(
+              title: "Flutter Devloper",
+              body: "It working",
+            );
+          },
+          icon: Icons.notifications,
+          text: "Instance Notification",
+        ),
+        commonButton(
+          onPressed: () => NotificationService().scheduleNotificationCancel(),
+          icon: Icons.cancel,
+          text: "Schedule Cancel",
+        ),
       ]),
     );
   }
+}
+
+Widget commonButton({required VoidCallback onPressed, required IconData icon, required String text}) {
+  return Padding(
+    padding: EdgeInsets.symmetric(horizontal: 50.w),
+    child: ElevatedButton(
+      onPressed: onPressed,
+      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Icon(icon),
+        SizedBox(width: 10.w),
+        Text(text),
+      ]),
+    ),
+  );
 }
